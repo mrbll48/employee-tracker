80fcd50aff0b3b972a865c17d3ddf7de3793166d
mrbll48 <william.marks87@gmail.com>
2023-06-06 13:32:10 -0700

    fixed error in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,24 @@
-DROP DATABASE IF EXIST employee_db;
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 use employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     dept_name VARCHAR(50)
-    ON DELETE SET NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     job_title VARCHAR(50),
     department VARCHAR(50)
-    ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+    id INT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     employee_salary INT NOT NULL,
     department VARCHAR(50),
     managers VARCHAR (50)
-    ON DELETE SET NULL
 );
